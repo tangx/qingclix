d2dc0c4c7f9d8d0126a250df48a682eea4b1d5fb
tangx <uyinn@live.com>
2020-04-11 00:30:26 +0800

    add: volume attachVolume req and resp struct

--- resources/volume.go
+++ resources/volume.go
@@ -8,3 +8,15 @@ type VolumeRequest struct {
 	Months     string `json:"months,omitempty" yaml:"months,omitempty" url:"months,omitempty"`
 	AutoRenew  string `json:"auto_renew,omitempty" yaml:"auto_renew,omitempty" url:"auto_renew,omitempty"`
 }
+
+type AttachVolumesResquest struct {
+	Volumes  []string `yaml:"volumes,omitempty" json:"volumes,omitempty" url:"volumes,omitempty"`
+	Instance string   `yaml:"instance,omitempty" json:"instance,omitempty" url:"instance,omitempty"`
+	Zone     string   `yaml:"zone,omitempty" json:"zone,omitempty" url:"zone,omitempty"`
+}
+
+type AttachVolumesResponse struct {
+	Action  string `yaml:"action,omitempty" json:"action,omitempty"`
+	JobID   string `yaml:"job_id,omitempty" json:"job_id,omitempty"`
+	RetCode string `yaml:"ret_code,omitempty" json:"ret_code,omitempty"`
+}
