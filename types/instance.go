7b957a897638257ed06696074d89d135cc55aad7
tangx <uyinn@live.com>
2020-04-14 17:57:52 +0800

    update: 支持 instance 绑定多个硬盘

--- types/instance.go
+++ types/instance.go
@@ -11,6 +11,7 @@ type RunInstancesRequest struct {
 	InstanceName  string   `yaml:"instance_name,omitempty" json:"instance_name,omitempty" url:"instance_name,omitempty"`
 	Zone          string   `yaml:"zone,omitempty" json:"zone,omitempty" url:"zone,omitempty"`
 	Vxnets        []string `yaml:"vxnets,omitempty" json:"vxnets,omitempty" url:"vxnets,omitempty,dotnumbered,numbered1"`
+	InstanceType  string   `yaml:"instance,omitempty" json:"instance_type,omitempty" url:"instance_type,omitempty"`
 }
 
 type RunInstancesResponse struct {
@@ -43,20 +44,21 @@ type DescribeInstancesResponse struct {
 }
 
 type InstanceSet struct {
-	Vxnets           []Vxnet  `json:"vxnets"`
-	MemoryCurrent    int      `json:"memory_current"`
-	VcpusCurrent     int      `json:"vcpus_current"`
-	Image            Image    `json:"image"`
-	InstanceName     string   `json:"instance_name"`
-	InstanceClass    int      `json:"instance_class"`
-	Status           string   `json:"status"`
-	Description      string   `json:"description"`
-	ReservedContract string   `json:"reserved_contract"`
-	VolumeIDS        []string `json:"volume_ids"`
-	ZoneID           string   `json:"zone_id"`
-	InstanceID       string   `json:"instance_id"`
-	InstanceType     string   `json:"instance_type"`
-	Volumes          []Volume `json:"volumes"`
+	Vxnets           []Vxnet  `json:"vxnets,omitempty"`
+	MemoryCurrent    int      `json:"memory_current,omitempty"`
+	VcpusCurrent     int      `json:"vcpus_current,omitempty"`
+	Image            Image    `json:"image,omitempty"`
+	InstanceName     string   `json:"instance_name,omitempty"`
+	InstanceClass    int      `json:"instance_class,omitempty"`
+	Status           string   `json:"status,omitempty"`
+	Description      string   `json:"description,omitempty"`
+	ReservedContract string   `json:"reserved_contract,omitempty"`
+	VolumeIDS        []string `json:"volume_ids,omitempty"`
+	ZoneID           string   `json:"zone_id,omitempty"`
+	InstanceID       string   `json:"instance_id,omitempty"`
+	InstanceType     string   `json:"instance_type,omitempty"`
+	Volumes          []Volume `json:"volumes,omitempty"`
+	KeypairIDS       []string `json:"keypair_ids,omitempty"`
 }
 
 type Image struct {
