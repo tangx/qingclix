a983fcf53fc76e4f92e6dd0bc0c917f7068546a1
tangx <uyinn@live.com>
2020-04-15 15:18:33 +0800

    modify: 将 config load and save 函数从 root.go 移动到 instance.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,12 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"path/filepath"
-	"sort"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/tangx/qingclix/global"
-	"github.com/tangx/qingclix/utils"
-	"gopkg.in/AlecAivazis/survey.v1"
 )
 
 var rootCmd = &cobra.Command{
@@ -43,96 +36,4 @@ func Execute() {
 func SetLogLevel(level int) {
 	logLevel := logrus.Level(level)
 	logrus.SetLevel(logLevel)
-}
-
-// LoadPresetConfig 读取预设配置
-func LoadPresetConfig() PresetConfig {
-	body, err := ioutil.ReadFile(global.ConfigFile)
-	logrus.Debugf("%s", body)
-
-	if err != nil {
-		ConfigHelp()
-		logrus.Fatal(err)
-	}
-	// fmt.Printf("%s\n", body)
-
-	var preset PresetConfig
-	err = json.Unmarshal(body, &preset)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	// fmt.Println(preset)
-
-	return preset
-}
-
-// ChooseItem 选择预设配置
-func ChooseItem(preset PresetConfig) ItemConfig {
-
-	var option []string
-	for k := range preset.Configs {
-		option = append(option, k)
-	}
-	// 结果排序，优化展示效果
-	sort.Strings(option)
-
-	// 选择
-	var qs = []*survey.Question{
-		{
-			Name: "choice",
-			Prompt: &survey.Select{
-				Message: "选择购买配置: ",
-				Options: option,
-			},
-		},
-	}
-	var choice string
-	err := survey.Ask(qs, &choice)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return preset.Configs[choice]
-}
-
-// 保存配置文件
-func SaveConfigToFile(preset PresetConfig) {
-	data, err := json.MarshalIndent(preset, "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	err = ioutil.WriteFile(global.ConfigFile, data, 0644)
-	if err != nil {
-		logrus.Errorln(err)
-	}
-}
-
-// InitialConfig 初始化配置
-func InitialConfig() {
-	base := `{"configs":{}}`
-
-	_, err := utils.MkdirAll(filepath.Dir(global.ConfigFile))
-
-	if err != nil {
-		logrus.Infoln(err)
-	}
-
-	err = ioutil.WriteFile(global.ConfigFile, []byte(base), 0644)
-	if err != nil {
-		logrus.Fatalln(err)
-	}
-}
-
-func ConfigHelp() {
-	usage := `Help:
-请检查配置文件[%s] 
-1. 是否存在
-2. Json/YAML 结构是否正确
-
-或使用 qingclix instance configure --initial 初始化
-  注意: 该命令会清空、原始数据。
-
-
-`
-	fmt.Printf(usage, global.ConfigFile)
 }
