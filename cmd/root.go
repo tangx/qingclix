4b74691adf0c2f15d85b6df8ebd26ebc611e852b
tangx <uyinn@live.com>
2020-04-15 13:57:00 +0800

    modify: 更新func 名字

--- cmd/root.go
+++ cmd/root.go
@@ -61,7 +61,7 @@ func LoadPresetConfig() PresetConfig {
 }
 
 // 保存配置文件
-func WriteConfig(preset PresetConfig) {
+func SaveConfigToFile(preset PresetConfig) {
 	data, err := json.MarshalIndent(preset, "", "  ")
 	if err != nil {
 		panic(err)
