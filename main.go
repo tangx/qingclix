dc4997f4881090e24f6c575639cedd3e896ba726
tangx <uyinn@live.com>
2020-05-07 17:31:36 +0800

    chore: 删除多余无用代码

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
 	"github.com/tangx/qingclix/cmd"
-	"github.com/tangx/qingclix/global"
 )
 
 func main() {
-	level := logrus.Level(global.Verbose)
-	logrus.SetLevel(level)
-
 	cmd.Execute()
 
 }
