72cf6b2772be2b441440ca8592d9e2875c32ae04
tangx <uyinn@live.com>
2020-05-07 14:09:39 +0800

    feat: 实现多实例购买及起始编号设置

--- cmd/instance.go
+++ cmd/instance.go
@@ -31,14 +31,38 @@ to quickly create a Cobra application.`,
 	},
 }
 
+var (
+	instance_count int
+	start_with     int
+)
+
 func init() {
 	rootCmd.AddCommand(instanceCmd)
 	instanceCmd.AddCommand(instanceCmdrun)
+	instanceCmdrun.Flags().IntVarP(&instance_count, "count", "c", 1, "新购实例数量")
+	instanceCmdrun.Flags().IntVarP(&start_with, "start-with", "", 0, "服务器名称起始编号")
+
 }
 
 func LaunchMain() {
+
 	item := chooseItem()
 
+	instName := item.Instance.InstanceName
+
+	for i := 0; i < instance_count; i++ {
+		if i == 0 {
+			item.Instance.InstanceName = instName + fmt.Sprintf("-%d", start_with)
+		} else {
+			item.Instance.InstanceName = instName + fmt.Sprintf("-%d", i+start_with)
+		}
+
+		logrus.Infoln(item.Instance.InstanceName)
+		runInstance(item)
+	}
+}
+
+func runInstance(item ClixItem) {
 	// run instance
 
 	// struct 强制类型转换 ， 此处不可用 :
@@ -67,8 +91,14 @@ func LaunchMain() {
 	// create volumes
 	vols := []string{}
 	for i, v := range item.Volumes {
+
+		volname := instParams.InstanceName
+		if i != 0 {
+			volname = fmt.Sprintf("%s-%d", instParams.InstanceName, i)
+		}
+
 		params := qingyun.CreateVolumesRequest{
-			VolumeName: fmt.Sprintf("%s-%d", instParams.InstanceName, i),
+			VolumeName: volname,
 			VolumeType: v.VolumeType,
 			Size:       v.Size,
 			Zone:       item.Instance.Zone,
